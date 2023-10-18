edb4d90e1dd9ef3fded45fd86b3d2883a07c9d93
lzj <1170821870@qq.com>
2023-10-18 17:35:29 +0800

    新增Delegate反填，但数据项有限，具体反填历史库还没对接

---
+++ LimitSurface/CDelegateEdit.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "CCommonHead.h"
+#include <QtWidgets/QItemDelegate>
+#include <QtWidgets/QLineEdit>
+class CDelegateEdit : public QItemDelegate
+{
+public:
+	CDelegateEdit(QObject* parent = nullptr);
+	~CDelegateEdit();
+
+public:
+    // editing
+    //创建一个可编辑的框
+    QWidget* createEditor(QWidget* parent,const QStyleOptionViewItem& option,const QModelIndex& index) const;
+    //从model数据项获取数据填到编辑条中
+    void setEditorData(QWidget* editor, const QModelIndex& index) const;
+    //手动写编辑框会回填到model项中，在回写到想要的数据内存
+    void setModelData(QWidget* editor, QAbstractItemModel* model, const QModelIndex& index) const;
+    //框的一个显示浮起，不加也行
+    void updateEditorGeometry(QWidget* editor,const QStyleOptionViewItem& option,const QModelIndex& index) const;
+
+};
+
--- LimitSurface/CTableModel.h
+++ LimitSurface/CTableModel.h
@@ -3,6 +3,7 @@
 #include "CTaos.h"
 #include "CGlobal.h"
 #include "CTaosMeasTable.h"
+#include "CTaosMeasTag.h"
 class CTableModel:public QAbstractTableModel
 {
 public:
@@ -20,6 +21,12 @@ public:
 	//表格数据和头部
 	virtual QVariant data(const QModelIndex& index, int role = Qt::DisplayRole) const;
 	virtual QVariant headerData(int section, Qt::Orientation orientation,int role = Qt::DisplayRole) const;
+
+
+	//下面两个为代理回写函数
+	virtual bool setData(const QModelIndex& index, const QVariant& value, int role = Qt::EditRole);
+	//启用一下combobox和edit模型
+	Qt::ItemFlags flags(const QModelIndex& index) const;
 
 
 public:
